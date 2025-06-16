ef5bdb05e0cc8631ebc629aa1d2e56209b33d6ee
Pablo <pjmd89@gmail.com>
2025-06-16 11:48:06 -0400

    up

--- lib/infraestructure/connection/connection.dart
+++ lib/infraestructure/connection/connection.dart
@@ -1,12 +1,13 @@
-import 'dart:developer';
-
 import 'package:agile_front/infraestructure/connection/connection_type_enum.dart';
+
+import 'service.dart';
 
 class Connection {
   ConnectionTypeEnum type;
-  Service connectionService = Service();
+  Service connectionService;
   Connection({
     this.type = ConnectionTypeEnum.graphql,
+    required this.connectionService,
   }) {
     this.type = type;
     
