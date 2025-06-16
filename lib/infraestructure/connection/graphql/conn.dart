dd963ad478d923f8e6292ffb29cf330e2baaa67c
Pablo <pjmd89@gmail.com>
2025-06-16 12:44:57 -0400

    up

--- lib/infraestructure/connection/graphql/conn.dart
+++ lib/infraestructure/connection/graphql/conn.dart
@@ -6,7 +6,7 @@ class GraphqlConn extends Service{
   final GraphQLClient _client;
   GraphqlConn({required GraphQLClient client}) : _client = client;
   @override
-  operation({required op.Operation operation, required void Function(Object) callback, Map<String, dynamic>? variables}) async{
+  Future<dynamic> operation({required op.Operation operation, required dynamic Function(Object) callback, Map<String, dynamic>? variables}) async{
     final queryVariables = variables ?? {};
     final response = await _client.query(
       QueryOptions(
@@ -22,6 +22,6 @@ class GraphqlConn extends Service{
       }
       return;
     }
-    callback(operation.result(response.data ?? {}));
+    return await callback(operation.result(response.data ?? {}));
   }
 }
--- lib/infraestructure/connection/service.dart
+++ lib/infraestructure/connection/service.dart
@@ -1,7 +1,7 @@
 import 'package:agile_front/infraestructure/operation.dart';
 
 abstract class Service{
-  operation({
+  Future<dynamic> operation({
     required Operation operation,
     required Function(Object) callback,
     Map<String, dynamic>? variables,
