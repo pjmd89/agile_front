bc0789201b70873754f397eb9b9bfd260760a067
Pablo <pjmd89@gmail.com>
2025-06-20 11:36:17 -0400

    up

--- lib/agile_front.dart
+++ lib/agile_front.dart
@@ -1,3 +1,4 @@
 export 'package:agile_front/infraestructure/connection/graphql/graphql_conn.dart';
 export 'package:agile_front/infraestructure/service.dart';
 export 'package:agile_front/infraestructure/operation.dart';
+export 'package:agile_front/infraestructure/usecase.dart';
