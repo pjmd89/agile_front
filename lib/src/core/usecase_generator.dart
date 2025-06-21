76104621a9471c409618fa2ad948269529705487
Pablo <pjmd89@gmail.com>
2025-06-21 16:14:48 -0400

    up

--- lib/src/core/usecase_generator.dart
+++ lib/src/core/usecase_generator.dart
@@ -106,7 +106,7 @@ class PageGenerator {
         }
         final classCrud = crud == 'read' ? '' : _capitalize(crud);
         final className = '${_capitalize(name)}${classCrud}Page';
-        final viewModelImport = "import '/src/presentation/pages/$name/view_model.dart';";
+        final viewModelImport = "import '/src/presentation/pages/$name/$crud/view_model.dart';";
         final file = File('${crudDir.path}/main.dart');
         if (!file.existsSync()) {
           file.writeAsStringSync('''import 'package:flutter/material.dart';
