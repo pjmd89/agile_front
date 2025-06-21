cb873205df5f760a5d6846d27c496caed1a6f0d8
Pablo <pjmd89@gmail.com>
2025-06-21 02:07:48 -0400

    up

--- lib/src/core/templates/notifier/gql_notifier.dart
+++ lib/src/core/templates/notifier/gql_notifier.dart
@@ -1,11 +1,12 @@
 
 String appGqlNotifier = r'''import 'package:agile_front/agile_front.dart';
 import 'package:flutter/material.dart';
+import '/src/infraestructure/config/env.dart';
 
 class GQLNotifier extends ChangeNotifier {
   final GqlConn gqlConn = GqlConn(
-    apiURL: 'http://localhost:8081',
-    wsURL: null,
-    insecure: false,
+    apiURL: Environment.backendApiUrl,
+    wsURL: Environment.backendApiUrlWS,
+    insecure: Environment.env == EnvEnum.dev,
   );
 }''';
