8b202fa34f1ac096aa5fd1765f0b69c138451f3e
Pablo <pjmd89@gmail.com>
2025-06-21 01:15:42 -0400

    up

--- lib/src/core/templates/loading_notifier.dart
+++ lib/src/core/templates/loading_notifier.dart
@@ -1,8 +1,6 @@
 String loadingNotifier= r'''import 'package:flutter/material.dart';
-
 class LoadingNotifier extends ChangeNotifier {
   bool _loading = false;
-
   bool get loading => _loading;
   
   Future <void> setLoading(bool loading, {bool notify = true}) async{
