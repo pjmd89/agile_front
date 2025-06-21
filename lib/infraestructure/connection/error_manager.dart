475e62551244c9e57319e853b3a72ce5cd6d263e
Pablo <pjmd89@gmail.com>
2025-06-21 12:23:01 -0400

    up

--- lib/infraestructure/connection/error_manager.dart
+++ lib/infraestructure/connection/error_manager.dart
@@ -1,5 +1,6 @@
 import 'package:graphql/client.dart';
-
+export 'package:graphql/client.dart'
+    show GraphQLError, QueryResult;
 interface class ErrorConnManager{
   void handleGraphqlError(List<GraphQLError> errors) {}
   void handleHttpError(QueryResult result) {}
